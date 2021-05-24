74ae4994871124be27346a327abe9d6898bd3c3e
delfinopaulaemerson <delfs@hotmail.com>
2021-05-24 12:42:32 -0300

    start project

---
+++ src/test/java/com/emersondelfino/ApiRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emersondelfino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
